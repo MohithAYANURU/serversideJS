3c46035b823b8925b7758f312f9e3ab8d6613625
lostmart <martinpedraza1979@gmail.com>
2026-04-07 13:43:06 +0200

    feat: static files added

--- BACK/index.js
+++ BACK/index.js
@@ -13,6 +13,7 @@ const port = 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+app.use(express.static('public'))
 
 // TODO 4: Mount studentRouter at "/api/students"
 // Hint: app.use("/api/students", studentRouter)
